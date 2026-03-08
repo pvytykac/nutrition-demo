8cb963a8e0615e32084447beb5f567eb08ad17cb
paly <pvytykac@gmail.com>
2026-03-08 15:23:42 +0100

    chore: removed unused imports

--- backend/src/main/java/net/pvytykac/nutrition/ingredient/IngredientController.java
+++ backend/src/main/java/net/pvytykac/nutrition/ingredient/IngredientController.java
@@ -5,8 +5,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.pvytykac.nutrition.util.filtering.NumericFilter;
-import net.pvytykac.nutrition.util.filtering.StringFilter;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
