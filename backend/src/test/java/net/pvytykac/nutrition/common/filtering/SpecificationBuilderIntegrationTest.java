f9a0771803d88cd435600abe035f21924c14c952
paly <pvytykac@gmail.com>
2026-06-30 18:32:48 +0200

    fix: replaced nullaway @SuppressWarnings with test source exclusion

--- backend/src/test/java/net/pvytykac/nutrition/common/filtering/SpecificationBuilderIntegrationTest.java
+++ backend/src/test/java/net/pvytykac/nutrition/common/filtering/SpecificationBuilderIntegrationTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SuppressWarnings("NullAway")
 @DisplayName("SpecificationBuilder Integration")
 class SpecificationBuilderIntegrationTest extends RepositoryTestBase {
 
